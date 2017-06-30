e776d6df48253a2eaf373c5a7bdc86c2117e278c
unknown <hanan.neriya@gmail.com>
2017-06-29 20:39:45 -0700

    up to contact form

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){ 
 	$(".scroll").click(function(event){
 		event.preventDefault();
-		$("html,body").animate({scrollTop:$(this.hash).offset().top}, 500);
+		$("html,body").animate({scrollTop:$(this.hash).offset().top -100}, 500);
 		$('.navbar-default a').removeClass('selected');
 		$(this).addClass('selected');
     	});
